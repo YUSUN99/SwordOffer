45fa0ecb2dcf18604ea87b5b32341c1554836134
YUSUN99 <2574110027@qq.com>
2021-12-12 18:10:59 +0800

    JZ30

--- source/main.cpp
+++ source/main.cpp
@@ -4,13 +4,15 @@
 #include<string>
 #include"JZ52_FindFirstCommonNode.h"
 #include"JZ09_CQueue.h"
+#include"JZ30_MinStack.h"
 
 using namespace std;
 
 int main()
 {
 	//test_JZ52_FindFirstCommonNode();
-	test_JZ09_CQueue();
+	//test_JZ09_CQueue();
+	test_JZ30_MinStack();
 
 	getchar();
 	return 0;
